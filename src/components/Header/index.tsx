a3fc3536b89e598ac0c8ba759f926bf6baf9e128
xabierlameiro.com <xabier.lameiro@gmail.com>
2023-02-11 20:43:10 +0000

    Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,21 @@
-const Header = () => <></>;
+import Link from 'next/link';
+import styles from './Header.module.css';
+
+type HeaderProps = {
+    isLoading?: boolean;
+};
+
+const Header = ({ isLoading = true }: HeaderProps) => {
+    return (
+        <header data-testid="podcast-header">
+            <nav className={styles.header_nav}>
+                <Link href="/" className={styles.header_nav_link}>
+                    Podcaster
+                </Link>
+                {isLoading && <span data-testid="podcast-header-mark" className={styles.header_nav_mark} />}
+            </nav>
+        </header>
+    );
+};
 
 export default Header;
