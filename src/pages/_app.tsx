a66f6ed04f56c7c577e9e0004ae7aa5edced724b
xabierlameiro.com <xabier.lameiro@gmail.com>
2023-02-12 13:02:56 +0000

    Add dedupingInterval to use cache for 24 hours

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,45 +1,27 @@
 import React from 'react';
-import { SWRConfig } from 'swr';
+import { SWRConfig, useSWRConfig } from 'swr';
 import Layout from '@/components/Layout';
 import type { AppProps } from 'next/app';
 import '../styles/global.css';
 
-export const localStorageProvider = () => {
-    // if the window hasn't loaded yet (SSR workaround)
-    if (typeof window === `undefined`) {
-        return new Map();
-    }
-    const map = new Map(JSON.parse(localStorage.getItem(`app-cache`) || `[]`));
-
-    window.addEventListener(`beforeunload`, () => {
-        const appCache = JSON.stringify(Array.from(map.entries()));
-        localStorage.setItem(`app-cache`, appCache);
-    });
-    return map;
-};
-
 export default function App({ Component, pageProps }: AppProps) {
     const [loading, setLoading] = React.useState(false);
+    const { cache } = useSWRConfig();
 
     return (
         <SWRConfig
             value={{
-                fetcher: async (url: string) => {
-                    const cache = localStorageProvider();
+                fetcher: async (url: string, ...rest) => {
                     const cachedData = cache.get(url);
-                    if (!cachedData || !cachedData.data) {
+                    if (cachedData && !cachedData.data) {
                         setLoading(true);
                     }
-                    return await fetch(url)
-                        .then((res) => res.json())
-                        .finally(() => setLoading(false));
+                    const res = await fetch(url, ...rest);
+                    setLoading(false);
+                    return res.json();
                 },
-                provider: () => localStorageProvider(),
-                revalidateOnFocus: false,
-                revalidateOnReconnect: false,
-                refreshInterval: 1000 * 60 * 60 * 24,
-                refreshWhenOffline: false,
-                refreshWhenHidden: false,
+                // We do not make the same call until 24 hours have passed.
+                dedupingInterval: 1000 * 60 * 60 * 24,
             }}
         >
             <Layout isLoading={loading}>
